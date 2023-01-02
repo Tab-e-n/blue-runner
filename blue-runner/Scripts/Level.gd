93b3ced8d174681f35c2fa6f9122cad4eaa109e1
Tab-e-n <howdoesonename@gmail.com>
2023-01-02 13:15:18 +0100

    update_moment

--- blue-runner/Scripts/Level.gd
+++ blue-runner/Scripts/Level.gd
@@ -14,7 +14,7 @@ onready var global : Control = $"/root/Global"
 
 var time : float = 0
 var par : float = 0
-var level_base : Array
+var level_base : Array = ["",""]
 
 func _ready():
 	#reload()
@@ -35,12 +35,24 @@ func reload():
 	if temp_level_symbol != null: level_symbol = temp_level_symbol
 	$symbol.texture = level_symbol
 	
-	if level_base[1]+level_base[0] != temp["level_base"][1]+temp["level_base"][0]:
+	if temp.has("level_base"): if level_base[1]+level_base[0] != temp["level_base"][1]+temp["level_base"][0]:
+		var loadfile = File.new()
+		var loaded = {}
 		
-		level_normal = load(temp["level_icon"][1] + "/Visual/Level/" + temp["level_icon"][0] + ".png")
-		level_locked
-		level_done
-		level_perfect
+		if loadfile.file_exists(temp["level_base"][1] + "/Visual/Level/base.dat"): # does file exist
+			loadfile.open(temp["level_base"][1] + "/Visual/Level/base.dat", File.READ)
+			
+			while loadfile.get_position() < loadfile.get_len():
+				var parsedData = parse_json(loadfile.get_line())
+				
+				loaded = parsedData
+			
+			loadfile.close()
+		
+		level_normal = load(temp["level_icon"][1] + "/Visual/Level/" + loaded[temp["level_base"][0]][0])
+		level_locked = load(temp["level_icon"][1] + "/Visual/Level/" + loaded[temp["level_base"][0]][1])
+		level_done = load(temp["level_icon"][1] + "/Visual/Level/" + loaded[temp["level_base"][0]][2])
+		level_perfect = load(temp["level_icon"][1] + "/Visual/Level/" + loaded[temp["level_base"][0]][3])
 	
 	$icon.texture = level_normal
 	$boltcollect.visible = false
