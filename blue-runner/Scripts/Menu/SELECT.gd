94fe215889ddc37e505a53bfb5ba8d3cbf64ee97
Tab-e-n <howdoesonename@gmail.com>
2023-06-13 23:01:11 +0200

    spinkat

--- blue-runner/Scripts/Menu/SELECT.gd
+++ blue-runner/Scripts/Menu/SELECT.gd
@@ -3,4 +3,11 @@ extends Node2D
 onready var parent : Node2D = get_parent()
 
 func menu_update():
-	pass
+	if Input.is_action_just_pressed("left"):
+		$levels.position.x += 192
+	if Input.is_action_just_pressed("right"):
+		$levels.position.x -= 192
+	if Input.is_action_just_pressed("up"):
+		$levels.position.y += 128
+	if Input.is_action_just_pressed("down"):
+		$levels.position.y -= 128
--- blue-runner/Scripts/Menu/Scrolling_Text.gd
+++ blue-runner/Scripts/Menu/Scrolling_Text.gd
@@ -1,6 +1,7 @@
 extends Control
 
 export var default_text : String = ""
+export var line_number : int = 1
 export var scroll_speed : float = 1
 export var pause_time : int = 60
 
@@ -27,7 +28,42 @@ func _physics_process(_delta):
 func set_text(text, rect_pos = 0, paused = pause_time, new_direction = -1):
 	$text.text = text
 	$text.rect_size.x = $text.get_font("normal_font").get_string_size($text.text).x
+	
+	if line_number > 1:
+		var scaling : int = ceil(($text.rect_size.x * $text.rect_scale.x) / rect_size.x)
+		print(scaling)
+		if scaling != 1:
+			var fake_line_number : int
+			if scaling < line_number:
+				fake_line_number = scaling
+			elif scaling >= line_number:
+				fake_line_number = line_number
+			
+			var line_character : int = text.length() / fake_line_number
+			var lines : Array = range(fake_line_number)
+			var last_pos : int = 0
+			for i in range(fake_line_number - 1):
+				var empty_pos : int = text.find(" ", line_character * (i + 1)) + 1
+				if empty_pos == -1:
+					empty_pos = line_character * (i + 1)
+				var line_text = text.substr(last_pos, empty_pos)
+				lines[i] = line_text
+				last_pos = empty_pos
+			lines[fake_line_number - 1] = text.substr(last_pos, text.length() - 1)
+			#print(lines)
+			var longest_line = lines[0]
+			text = lines[0]
+			for i in range(fake_line_number - 1):
+				text += "\n" + lines[i + 1]
+				if lines[i + 1].length() > longest_line.length():
+					longest_line = lines[i + 1]
+			#print(longest_line)
+			
+			$text.text = text
+			$text.rect_size.x = $text.get_font("normal_font").get_string_size(longest_line).x
+	
 	size_difference = ($text.rect_size.x * $text.rect_scale.x) - rect_size.x
+	
 	$text.rect_position.x = rect_pos
 	paused_timer = paused
 	direction = new_direction
